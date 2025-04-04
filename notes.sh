7f728d538cf23e3e392ea2ee9cbf103cd427330f
HiruyWorku <153773845+HiruyWorku@users.noreply.github.com>
2025-04-04 13:32:16 -0400

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -1,1 +1,6 @@
 - vi analyze-one-dev.sh
+- author="Jessica Castelino <js519934@dal.ca>"
+output_file="java_analysis_temp/jessica_java_commits.txt"
+
+- author="Bhumika Sharma <bhumikas30@gmail.com>"
+output_file="java_analysis_temp/bhumika_java_commits.txt"
